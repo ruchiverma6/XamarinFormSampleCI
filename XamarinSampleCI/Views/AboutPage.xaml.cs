2c56783262d774ab552c13bfcc4bdf3068f4f163
Ruchi Devi <ruchidevi@Admins-MacBook-Pro.local>
2018-11-22 16:25:59 +0530

    First Commit

---
+++ XamarinSampleCI/Views/AboutPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinSampleCI.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
